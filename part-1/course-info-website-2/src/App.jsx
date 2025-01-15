ec69a4cb8b49027252a713a2e48dcb6bf42f4b0c
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-15 11:37:51 -0800

    Make module for components

--- part-1/course-info-website-2/src/App.jsx
+++ part-1/course-info-website-2/src/App.jsx
@@ -1,3 +1,5 @@
+import Home from "./components/Home"
+
 const App = () => {
   const courses = [
     {
@@ -47,39 +49,5 @@ const App = () => {
 
   return <Home courses={courses} />
 }
-
-const Home = ({courses}) => {
-  return (
-    <div>
-      {courses.map(course => <Course course={course} key={course.id}/>)}
-    </div>
-  );
-}
-
-const Course = ({course}) => {
-  course.totalExerciseCount = course.parts.reduce((a, p) => a + p.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course}/>
-      <Content parts={course.parts}/>
-      <Total sum={course.totalExerciseCount}/>
-    </div>
-  );
-}
-
-const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Total = ({ sum }) => <strong><p>Total of {sum} exercises.</p></strong>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <div>
-      {parts.map(part => <Part key={part.id} part={part}/>)}
-  </div>
 
 export default App
