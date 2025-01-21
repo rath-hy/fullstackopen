278c0e85f7605e95881e17954ac7fbd17771efc7
Puthyrath Hy <puthyrathy@gmail.com>
2025-01-20 22:07:31 -0800

    Finalize 2.18

--- part-2/countries/src/App.jsx
+++ part-2/countries/src/App.jsx
@@ -6,56 +6,93 @@ import axios from 'axios'
 
 function App() {
   const [inputValue, setInputValue] = useState('');
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null);                       //all countries data
+  const [countriesToShow, setCountriesToShow] = useState(null); //filtered countries
+  const [country, setCountry] = useState(null);                 //the only possible country (show in depth info)
+
+  const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   }
-
-  const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
   
   useEffect(() => {
     axios
-      .get(baseUrl)
-      .then( (response) => {
-        // console.log(response.data);
+      .get(`${baseUrl}/all`)
+      .then((response) => {
         setData(response.data);
       })
-  }, []);
+  }, []); //reads in data for all countries once and never again
+
+  useEffect(() => {
+    if (data && inputValue) {
+      const countriesShowable = data
+        .filter(countryData => countryData.name.common.toLowerCase().includes(inputValue.toLowerCase()))
+        .map(countryData => 
+          <div key={countryData.name.common}>
+            {countryData.name.common}
+          </div>)
+        
+      setCountriesToShow(countriesShowable);
+    } else {
+      setCountriesToShow(null);
+    }
+  }, [inputValue]); //checks on countriesToShow every time input changes
+
+  useEffect(() => {
+    if (countriesToShow && countriesToShow.length === 1) {
+      axios
+        .get(`${baseUrl}/name/${countriesToShow[0].props.children}`) //countriesToShow is a list of <div>content</div>
+        .then(response => setCountry(response.data))
+    } else {
+      setCountry(null);
+    }
+  }, [countriesToShow]) 
 
   return (
-    <>
+    <div>
       Find countries
       <input value={inputValue} onChange={handleInputChange}/>
 
-      <CountryList data={data} inputValue={inputValue}/>
-    </>
+      <CountryList countriesToShow={countriesToShow} country={country}/>
+    </div>
   )
 }
 
-const CountryList = ({data, inputValue}) => {
-  if (!data) {
+//can use components to make the request function simpler
+const CountryList = ({countriesToShow, country}) => {
+  if (countriesToShow === null) {
     return null;
   }
 
-  const countriesShowable = data
-    .filter(countryData => countryData.name.common.toLowerCase().includes(inputValue.toLowerCase()))
-    .map(countryData => 
-      <div key={countryData.name.common}>
-        {countryData.name.common}
-      </div>)
+  if (countriesToShow.length > 10)
+    return <div>Too many matches; please specify another filter.</div>;
 
-  if (countriesShowable.length > 10) {
+  if (countriesToShow.length > 1)
+    return <div>{countriesToShow}</div>;
+
+  if (countriesToShow.length === 1 && country !== null) {
+    const languagesArray = Object.values(country.languages)
+    const languageCodesArray = Object.keys(country.languages)
+
     return (
       <div>
-        Too many matches; please specify another filter.
+          <h3>{country.name.official}</h3>
+          <div>Capital: {country.capital}</div>
+          <div>Area: {country.area}</div>
+          <div>Languages:</div>
+          <ul>
+            {
+              languagesArray.map((language, index) => 
+                <li key={languageCodesArray[index]}>{language}</li>)
+            }
+          </ul>
+          <img src={country.flags.png} width="auto" height="250"/>
       </div>
     );
   }
 
-  return (
-    <div>{countriesShowable}</div>
-  );
+  return <div>No matches.</div>
 }
 
 export default App
