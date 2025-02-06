f499a6192e8d902ee0380d437acb63472da957bf
Puthyrath Hy <puthyrathy@gmail.com>
2025-02-06 00:47:45 -0800

    Complete 4.17

---
+++ part-4/tests/user_api.test.js
@@ -0,0 +1,37 @@
+const { test, after, beforeEach, describe } = require('node:test');
+const assert = require('node:assert');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const User = require('../models/user');
+const api = supertest(app);
+
+after(async () => {
+    await mongoose.connection.close();
+});
+
+test('invalid users are not created', async () => {
+    const userWithTooShortUsername = {
+        "username": "jm",
+        "name": "James Madison",
+        "password": "jamesmadison123"
+    }
+
+    const userWithTooShortPassword = {
+        "username": "benjamin-franklin",
+        "name": "Benjamin Franklin",
+        "password": "bf"
+    }
+
+    await api 
+        .post('/api/users')
+        .send(userWithTooShortUsername)
+        .expect(400)
+
+    await api
+        .post('/api/users')
+        .send(userWithTooShortPassword)
+        .expect(400)
+
+    console.log('hey this was run ****')
+})
