4f404e611c8d5d8eaa403a4f78d3fb9918a15999
Puthyrath Hy <puthyrathy@gmail.com>
2025-02-07 19:28:12 -0800

    Complete 4.20

--- part-4/app.js
+++ part-4/app.js
@@ -5,6 +5,8 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
 
 
 const Blog = require('./models/blog')
@@ -14,8 +16,10 @@ const utils = require('./utils/config')
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl).then(() => info('Connected to mongoDB')).catch((error) => info('Error connecting to mongoDB', error.message))
 
+
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
