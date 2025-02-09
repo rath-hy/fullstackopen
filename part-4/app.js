0c087ae71d009118673b71175283194d8a0b0a6d
Puthyrath Hy <puthyrathy@gmail.com>
2025-02-08 20:45:31 -0800

    Complete 4.23

--- part-4/app.js
+++ part-4/app.js
@@ -20,10 +20,12 @@ mongoose.connect(mongoUrl).then(() => info('Connected to mongoDB')).catch((error
 app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
-// app.use(middleware.userExtractor)
-app.use('/api/blogs', blogsRouter)
+// app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.errorHandler) //i'm using it!!!
 
 module.exports = app
 
