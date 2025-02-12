be818349d05c6b7c4d3e18abda752d05b60786f6
Puthyrath Hy <puthyrathy@gmail.com>
2025-02-11 17:35:25 -0800

    Complete 5.4

---
+++ part-5/src/components/Notification.jsx
@@ -0,0 +1,31 @@
+const Notification = ({message, type}) => {
+    const errorStyle = {
+        color: 'blue',
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px'
+    }
+
+    if (type === 'success') {
+        errorStyle.color = 'green'
+    }
+
+    else if (type === 'error') {
+        errorStyle.color = 'red'
+    }
+
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div style={errorStyle}>
+      {message}
+    </div>
+  )
+}
+
+export default Notification
