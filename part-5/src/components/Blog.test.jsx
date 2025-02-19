f4bdcfc9aee000670a220107a13b7800bbe2b3b3
Puthyrath Hy <puthyrathy@gmail.com>
2025-02-18 16:12:07 -0800

    Complete 5.15

--- part-5/src/components/Blog.test.jsx
+++ part-5/src/components/Blog.test.jsx
@@ -6,6 +6,7 @@ import Blog from './Blog'
 describe('<Blog />', () => {
   let blog
   let container
+  const mockHandler = vi.fn()
 
   beforeEach(() => {
     blog = {
@@ -15,7 +16,7 @@ describe('<Blog />', () => {
       likes: 8
     }
 
-    container = render(<Blog blog={blog} />)
+    container = render(<Blog blog={blog} updateBlogLikes={mockHandler} />)
   })
 
   test('by default, renders title and author but not URL or like count', () => {
@@ -30,13 +31,21 @@ describe('<Blog />', () => {
     expect(screen.getByText(blog.likes, { exact: false })).not.toBeVisible()
 
     const user = userEvent.setup()
-    const button = screen.getByText('view')
-    await user.click(button)
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
     expect(screen.getByText(blog.url, { exact: false })).toBeVisible()
     expect(screen.getByText(blog.likes, { exact: false })).toBeVisible()
   })
 
+  test('if like button is clicked twice, corresponding event handler is called twice', async () => {
+    const user = userEvent.setup()
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
 })
 
 
