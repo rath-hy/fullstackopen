113c76fa98b887f80e320e7dfa0510f029c64a4e
Puthyrath Hy <puthyrathy@gmail.com>
2025-03-03 18:50:44 -0800

    Complete 5.22 and 5.23

--- part-5-tests/tests/helper.js
+++ part-5-tests/tests/helper.js
@@ -1,7 +1,20 @@
+const { expect } = require('@playwright/test')
+
 const loginWith = async (page, username, password)  => {
   await page.getByTestId('username-input').fill(username)
   await page.getByTestId('password-input').fill(password)
   await page.getByRole('button', { name: 'login' }).click()
 }
 
-export { loginWith }
+const createNewBlog = async (page, blog) => {
+  await page.getByRole('button', { name: 'new blog'} ).click()
+  await expect(page.getByText('Title')).toBeVisible()
+  
+  await page.getByTestId('title-input').fill(blog.title)
+  await page.getByTestId('author-input').fill(blog.author)
+  await page.getByTestId('url-input').fill(blog.url)
+
+  await page.getByRole('button', { name: 'create' }).click()
+}
+
+export { loginWith, createNewBlog }
