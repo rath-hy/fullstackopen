4836c3f32fb54daf9d2a647de6973e353f653390
Puthyrath Hy <puthyrathy@gmail.com>
2025-03-08 15:23:56 -0800

    Complete 6.9

--- part-6-b/src/components/AnecdoteList.jsx
+++ part-6-b/src/components/AnecdoteList.jsx
@@ -2,7 +2,18 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voter } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  // const anecdotes = useSelector(state => state.anecdotes)
+
+  const anecdotes = useSelector(state => {
+    if (state.filter === 'ALL') {
+      return state.anecdotes
+    }
+
+    else {
+      return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+    }
+
+  })
 
   const dispatch = useDispatch()
 
