5540869000ddb3d99a2ff0814ea6d21a7c94c737
Puthyrath Hy <puthyrathy@gmail.com>
2025-03-10 19:56:20 -0700

    Do 6.13

--- part-6-b/src/components/AnecdoteForm.jsx
+++ part-6-b/src/components/AnecdoteForm.jsx
@@ -1,13 +1,19 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
+
   const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    dispatch(createAnecdote({ content }))
+    dispatch(createAnecdote(content))
     event.target.anecdote.value = ''
+
+
+    const notification = `added new blog`
+    dispatch(setNotification(notification))
   }
 
     return (<>
--- part-6-b/src/reducers/notificationReducer.js
+++ part-6-b/src/reducers/notificationReducer.js
@@ -1,15 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = 'this is a notification'
+const initialState = 'blank notification'
 
 const notificationSlice = createSlice({
   name: 'notification',
   initialState,
 
   reducers: {
+    setNotification(state, action) {
+      return action.payload
+    },
+
+    removeNotification() {
+      return null
+    }
 
   }
 
 })
 
+export const { setNotification, removeNotification } = notificationSlice.actions
 export default notificationSlice.reducer
