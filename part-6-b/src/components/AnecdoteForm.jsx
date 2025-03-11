f598d08f717d09c89d922c7fd3439b35be63bdb2
Puthyrath Hy <puthyrathy@gmail.com>
2025-03-11 13:26:31 -0700

    Do 6.17

--- part-6-b/src/components/AnecdoteForm.jsx
+++ part-6-b/src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -11,11 +10,7 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
 
-    // dispatch(createAnecdote(content))
-  
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
-
+    dispatch(createAnecdote(content))
 
     const notification = `added new blog`
     dispatch(setNotification(notification))
