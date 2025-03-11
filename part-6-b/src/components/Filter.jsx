9e320836dcad07797ee65e89c8d2c1dd0f7d28f0
Puthyrath Hy <puthyrathy@gmail.com>
2025-03-10 17:28:07 -0700

    Do 6.10

--- part-6-b/src/components/Filter.jsx
+++ part-6-b/src/components/Filter.jsx
@@ -1,12 +1,11 @@
-import { filterChange } from "../reducers/filterReducer"
+import { setFilter } from "../reducers/filterReducer"
 import { useDispatch } from "react-redux"
 
 const Filter = () => {
   const dispatch = useDispatch()
 
   const handleChange = (event) => {
-    // console.log(event.target.value)
-    dispatch(filterChange(event.target.value))
+    dispatch(setFilter(event.target.value))
   }
   const style = {
     marginBottom: 10
