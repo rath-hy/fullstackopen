32065b6640475ad6a2f5e64fb22721442a32477e
Puthyrath Hy <puthyrathy@gmail.com>
2025-03-25 08:08:28 -0700

    Do 7.13

--- part-7-f/frontend/src/main.jsx
+++ part-7-f/frontend/src/main.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 //new imports
 import { NotificationContextProvider } from './contexts/NotificationContext'
+import { UserContextProvider } from './contexts/UserContext'
 import { QueryClient, QueryClientProvider }  from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -11,9 +12,10 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <NotificationContextProvider>
-      <App />
-    </NotificationContextProvider>
+    <UserContextProvider>
+      <NotificationContextProvider>
+        <App />
+      </NotificationContextProvider>
+    </UserContextProvider>
   </QueryClientProvider>
-
 )
