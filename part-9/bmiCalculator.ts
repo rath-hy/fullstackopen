f4edb2ef3a82e2a52ae80c859cdebda222279f29
Puthyrath Hy <puthyrathy@gmail.com>
2025-04-28 15:39:33 -0700

    Do 9.1

---
+++ part-9/bmiCalculator.ts
@@ -0,0 +1,22 @@
+const calculateBmi = (heightInCm: number, weightInKg: number): string => {
+  const heightInMeters: number = heightInCm / 100
+  const bmi: number = weightInKg / Math.pow(heightInMeters, 2)
+
+  switch(true) {
+    case (bmi <= 18.4):
+      return 'underweight'
+    case (bmi <= 24.9):
+      return 'normal range'
+    case (bmi <= 29.9):
+      return 'overweight'
+    case (bmi >= 29.9):
+      return 'obese'
+    default:
+      'error'
+  }
+}
+
+const heightInCm: number = Number(process.argv[2])
+const weightInKg: number = Number(process.argv[3])
+
+console.log(calculateBmi(heightInCm, weightInKg))
