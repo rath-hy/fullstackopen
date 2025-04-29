dd4f0d3565a43b5eb112ec71ac9529133e142879
Puthyrath Hy <puthyrathy@gmail.com>
2025-04-28 17:07:38 -0700

    Do 9.3

--- part-9/exerciseCalculator.ts
+++ part-9/exerciseCalculator.ts
@@ -64,24 +64,20 @@ const calculateExercises = (dailyExerciseHours: number[], target: number): Exerc
     target,
     average: dailyAverage
   }
-
 }
 
-// const dailyExerciseHours = JSON.parse(process.argv[2])
-// const target = Number(process.argv[3])
+const arguments = process.argv.slice(2)
+const target = Number(arguments[0])
+const dailyExerciseHours = arguments.slice(1).map(Number)
 
-const dailyExerciseHours = [3, 0, 2, 4.5, 0, 3, 1]
-const target = 2
+try {
+  console.log(calculateExercises(dailyExerciseHours, target))
+} catch (error: unknown) {
+  let errorMessage = 'Error: '
 
-console.log(calculateExercises(dailyExerciseHours, target))
+  if (error instanceof Error) {
+    errorMessage += error.message
+  }
 
-
-// { 
-//   periodLength: 7,
-//   trainingDays: 5,
-//   success: false,
-//   rating: 2,
-//   ratingDescription: 'not too bad but could be better',
-//   target: 2,
-//   average: 1.9285714285714286
-// }
+  console.log(errorMessage)
+}
